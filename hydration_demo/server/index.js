c1ccf8cf9b61aa0ee865d5d0fa3e5b0429da2e61
MdFazleRasool <fazleamity001@gmail.com>
2026-01-04 17:12:31 +0530

    Initial SSR Load working

--- hydration_demo/server/index.js
+++ hydration_demo/server/index.js
@@ -1,35 +1,39 @@
 import path from 'path';
-import fs from 'fs' ;
+import fs from 'fs';
 
-import React from 'react' ;
-import ReactDOMServer  from 'react-dom/server' ;
+import React from 'react';
+import ReactDOMServer from 'react-dom/server';
 
-import express from 'express' ;
-import App from '../src/App' ;
+import express from 'express';
+import App from '../src/App';
 
-const PORT = 3001 ;
+const PORT = 3001;
 
-const app  = express() ;
-app.get('/' , (req,res) => {
-    const appContent =  ReactDOMServer.renderToString(<App />) ;
-    const indexHTMLFile = path.resolve('./build/index.html') ;
-    fs.readFile(indexHTMLFile,'utf8' , (err,data) => {
-        if(err){
-            console.log("Something went wrong " , err);
-            return res.status(500).send('something went wrong') ;
+const app = express();
+app.get('/', (req, res) => {
+    const appContent = ReactDOMServer.renderToString(<App />);
+    const indexHTMLFile = path.resolve('./build/index.html');
+    fs.readFile(indexHTMLFile, 'utf8', (err, data) => {
+        if (err) {
+            console.log("Something went wrong ", err);
+            return res.status(500).send('something went wrong');
 
-            
+
         }
-        return res.send(data.replace(
-            '<div id="root" > </div>' ,`<div id="root">${appContent}</div>`
-        )) ;
+        return res.send(
+            data.replace(
+                '<div id="root"></div>',
+                `<div id="root">${appContent}</div>`
+            )
+        );
+
     })
 })
 
 
-app.use(express.static('./build')) ;
+app.use(express.static('./build'));
 
-app.listen(PORT , () =>{
+app.listen(PORT, () => {
     console.log(`app launched on ${PORT}`);
-    
+
 })
--- hydration_demo/src/App.js
+++ hydration_demo/src/App.js
@@ -1,23 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react' ;
 
 function App() {
+
+  const [tasks , setTasks ] = useState([]);
+  const [task , setTask ] = useState("");
+
+  function addTodo(e){
+    if(task){
+      setTasks([...tasks , task]) ;
+      setTask("") ;
+    }
+  }
+
+  
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>To Do App</h1>
+
+      <input 
+        type='text'
+        placeholder='Add a task'
+        onChange={(e) => setTask(e.target.value)}
+        value={task}
+      />
+      <button onClick={addTodo}>Add Task In List</button>
+
+      <ul>
+        {tasks.map((task,index) =>(
+          <li key={index}>{task}</li>
+        ))}
+      </ul>
     </div>
   );
 }

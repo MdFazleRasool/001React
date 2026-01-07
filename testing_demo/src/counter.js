8866377105f94fb90ee86e2b7a13a6cabc5f8b8b
MdFazleRasool <fazleamity001@gmail.com>
2026-01-07 12:06:43 +0530

    Learning unit testing Using Playwright

---
+++ testing_demo/src/counter.js
@@ -0,0 +1,11 @@
+import { useState } from "react";
+export default function Counter() {
+    const [count , setCount] = useState(0);
+
+    return(
+        <div>
+            <p  className="text-for-small-screen">Count :  <span   data-testid="count"> {count} </span></p>
+            <button onClick={() => setCount(count+1)}>Increment</button>
+        </div>
+    );
+}
---
+++ testing_demo/tests/example.spec.js
@@ -0,0 +1,24 @@
+// @ts-nocheck
+import { test, expect } from '@playwright/experimental-ct-react';
+import App from '../src/App';
+
+test('basic test', async ({ mount , page }) => {
+  const component = await mount(<App />) ;
+
+  const countSpan = await component.getByTestId('count') ;
+  // const countSpan = await component.getByTestId('count') ;
+
+  await component.getByRole('button' ,{name : 'Increment'}).click() ;
+
+  await expect(component).toContainText('Learn React') ;
+  await expect (countSpan).toHaveText('1');
+
+  
+
+
+
+
+
+});
+
+
